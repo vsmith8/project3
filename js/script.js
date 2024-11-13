5b0b2e3f80126d0621c3196ba51f50a2ea069b7d
vsmith8 <vsmith8@hawk.iit.edu>
2024-11-13 13:05:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 function initMap() {
-  // Get the map container by its ID
   var el = document.getElementById('map');
-
-  // Set the initial location for the map
   var myLocation = new google.maps.LatLng(42.403115, -86.275943);
 
-  // Map options
   var mapOptions = {
     center: myLocation,
     zoom: 16,
@@ -19,52 +15,46 @@ function initMap() {
     }
   };
 
-  // Create the map with the specified options
   var myMap = new google.maps.Map(el, mapOptions);
 
-  // Create a marker at the initial location
   var marker = new google.maps.Marker({
     position: myLocation,
     map: myMap,
     animation: google.maps.Animation.BOUNCE,
   });
 
-  // Content for the info window on the first marker
   var contentString = '<h1>West End of Downtown South Haven</h1><p>The western end of the South Haven downtown area. Just around the corner is this incredible hot dog restaurant called Chomp. I am starving right now. Send help. :( </p>';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
-
-  // Add a mouseover event listener to the first marker to show the info window
+  
   google.maps.event.addListener(marker, 'mouseover', function() {
     infowindow.open(myMap, marker);
   });
-
-  // Add a second marker at a different location (North Beach)
-  var secondLocation = new google.maps.LatLng(42.402640, -86.282971);
+  
+  // adding section location
+  var secondLocation = new google.maps.LatLng(42.402640, -86.282971); // North Beach
 
   var secondMarker = new google.maps.Marker({
     position: secondLocation,
     map: myMap,
-    animation: google.maps.Animation.DROP, // Drop animation
+    animation: google.maps.Animation.DROP, // Drop animation for the icon
   });
 
   var secondInfoWindow = new google.maps.InfoWindow({
     content: '<h1>North Beach and South Haven Pier</h1><p>This really fun part of town on the public beach with easy pier and harbor access.</p>'
   });
 
-  // Add mouseover event for the second marker to show its info window
   google.maps.event.addListener(secondMarker, 'mouseover', function() {
     secondInfoWindow.open(myMap, secondMarker);
   });
-
-  // Initialize a Street View panorama for the given location
+  
+  // add custom street view
   var streetViewControl = new google.maps.StreetViewPanorama(document.getElementById('street-view'), {
     position: myLocation,
     pov: { heading: 165, pitch: 0 }
   });
-
-  // Set the Street View on the map
+  
   myMap.setStreetView(streetViewControl);
 }
