7cd4173221d475579f44ac9865a94ed5a474e54b
vsmith8 <vsmith8@hawk.iit.edu>
2024-11-13 12:35:33 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function initMap() {
   var el = document.getElementById('map');
+  if (!el) {
+    console.error('Map element not found!');
+    return;
+  }
+
   var myLocation = new google.maps.LatLng(42.403115, -86.275943);
 
   var mapOptions = {
@@ -17,44 +22,43 @@ function initMap() {
 
   var myMap = new google.maps.Map(el, mapOptions);
 
+  // InfoWindow for the first marker
+  var infowindow = new google.maps.InfoWindow({
+    content: '<h1>My Location</h1><p>This is the location of the first marker.</p>'
+  });
+
   var marker = new google.maps.Marker({
     position: myLocation,
     map: myMap,
     animation: google.maps.Animation.BOUNCE,
   });
 
-  var contentString = '<h1>West End of Downtown South Haven</h1><p>The western end of the South Haven downtown area. Just around the corner is this incredible hot dog restaurant called Chomp. I am starving right now. Send help. :( </p>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-  
   google.maps.event.addListener(marker, 'mouseover', function() {
     infowindow.open(myMap, marker);
   });
-  
-  // adding section location
-  var secondLocation = new google.maps.LatLng(42.402640, -86.282971); // North Beach
+
+  // Second location: North Beach
+  var secondLocation = new google.maps.LatLng(42.402640, -86.282971);
 
   var secondMarker = new google.maps.Marker({
     position: secondLocation,
     map: myMap,
-    animation: google.maps.Animation.DROP, // Drop animation for the icon
+    animation: google.maps.Animation.DROP,
   });
 
   var secondInfoWindow = new google.maps.InfoWindow({
-    content: '<h1>North Beach and South Haven Pier</h1><p>This really fun part of town on the public beach with easy pier and harbor access.</p>'
+    content: '<h1>North Beach and South Haven Pier</h1><p>This is a fun part of town on the public beach with easy pier and harbor access.</p>'
   });
 
   google.maps.event.addListener(secondMarker, 'mouseover', function() {
     secondInfoWindow.open(myMap, secondMarker);
   });
-  
-  // add custom street view
+
+  // Adding Street View control
   var streetViewControl = new google.maps.StreetViewPanorama(document.getElementById('street-view'), {
     position: myLocation,
     pov: { heading: 165, pitch: 0 }
   });
-  
+
   myMap.setStreetView(streetViewControl);
 }
